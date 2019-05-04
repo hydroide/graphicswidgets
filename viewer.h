03e7306ab22d2766a2a1ef994baea137ca936805
Joe Yang <yangzhao3d@gmail.com>
2019-05-04 21:49:05 +0800

    move some code from viewer to processviewer

--- viewer.h
+++ viewer.h
@@ -22,44 +22,6 @@ public:
 protected:
     View *_view;
 
-    // page left begin
-    QWidget *_vbox10Widget;
-    QVBoxLayout *_vbox10;
-    QHBoxLayout *_hbox11;
-    QWidget *_hbox11Widget;
-    QSpacerItem *_spacer11;
-
-    QLabel *_label_selectYear;
-    QSpinBox *_sb_selectYear;
-    QPushButton *_pb_selectYear;
-
-    QLabel *_label_selectMonth;
-    QSpinBox *_sb_selectMonth;
-    QPushButton *_pb_selectMonth;
-
-    QLabel *_label_startDateTime;
-    QDateTimeEdit* _dte_startDateTime;
-
-    QLabel *_label_endDateTime;
-    QDateTimeEdit* _dte_endDateTime;
-
-    QPushButton *_viewOption;
-    QPushButton *_insertTo;
-    QPushButton *_generate;
-
-    QGridLayout *_grid;
-    QWidget *_gridWidget;
-
-    QSplitter *_splitter11;
-
-    QPushButton *_buttonPlus1;
-    ProcessXAxisWidget *_xAxis1;
-
-    QList<View *> _views;
-    QList<ProcessChart *> _processCharts;
-
-    QToolBar *_toolBar;
-
     // page right begin
     QWidget *_vbox20Widget;
     QVBoxLayout *_vbox20;
